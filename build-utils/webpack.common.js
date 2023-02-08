85dfb0e7ea2d765f0ec8d96c8caeee8c8e6d323b
Michael Carr <xyeres@me.com>
2023-02-08 15:36:56 -0800

    add: env support for dev and prod

---
+++ build-utils/webpack.common.js
@@ -0,0 +1,48 @@
+const path = require("path");
+
+const ESLintPlugin = require("eslint-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "..", "src/index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "..", "./dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new ESLintPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Plug-It-In Starter Template",
+      template: path.resolve(__dirname, "..", "./src/public/index.html"),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(jpg|png|jpeg)$/,
+        use: {
+          loader: "url-loader",
+        },
+      },
+      {
+        test: /\.(css)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  devServer: {
+    static: path.resolve(__dirname, "..", "./dist"),
+  },
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import React from "react";
-import pp from "./assets/images/pp-b-sm.jpg";
 import "./css/global.css";
 
 function App() {
-  return (
-    <h1>
-      React App
-      <img src={pp} alt="my pp" />
-    </h1>
-  );
+  return <h1>React App Ready</h1>;
 }
 
 export default App;
