773a29b244afc08eb42ee956ab40a75584559932
Michael Carr <xyeres@me.com>
2023-02-08 14:26:58 -0800

    Add docblock

---
+++ plug-it-in.php
@@ -0,0 +1,21 @@
+/**
+ * Plugin Name
+ *
+ * @package           PlugItIn 
+ * @author            Michael Carr
+ * @copyright         2023 Hero Creative
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Plug It In
+ * Description:       This is a starter plugin for WordPress.
+ * Version:           1.0.0
+ * Requires at least: 6.0
+ * Requires PHP:      8.0
+ * Author:            Michael Carr @ Hero Creative
+ * Author URI:        https://herocreative.com
+ * Text Domain:       plug-it-in
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://github.com/hero-creative/plug-it-in
+ */
